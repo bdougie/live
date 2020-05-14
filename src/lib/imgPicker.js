5a81f6ae839e69e2e254bedb2b4b21f7fa7267e9
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2020-05-14 12:01:28 -0700

    WIP imagepicker

---
+++ src/lib/imgPicker.js
@@ -0,0 +1,13 @@
+export default function imgPicker(name) {
+  switch (name) {
+    case name === 'friend':
+      return 'friend.png';
+    case name === 'action':
+      return 'action.png';
+    case name === 'sauced':
+      return 'sauced.png';
+    default:
+      return 'laptop.png';
+    // code block
+  }
+}
