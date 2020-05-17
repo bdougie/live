91f2ffa0fd4aa73da10f82063e39f1ab47968e59
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2020-05-17 06:55:05 -0700

    implement image picker

--- src/lib/imgPicker.js
+++ src/lib/imgPicker.js
@@ -1,13 +1,15 @@
-export default function imgPicker(name) {
-  switch (name) {
-    case name === 'friend':
-      return 'friend.png';
-    case name === 'action':
-      return 'action.png';
-    case name === 'sauced':
-      return 'sauced.png';
-    default:
-      return 'laptop.png';
-    // code block
-  }
+import laptop from '../../img/_laptop.jpg';
+import openSauced from '../../public/sauced2.png';
+import friend from '../../public/friend.png';
+import actions from '../../public/gh-actions.png';
+
+const images = {
+  friend: friend,
+  'open-sauced': openSauced,
+  actions: actions,
+};
+
+export default function imgPicker(labels) {
+  const label = labels.filter(x => x.node.name !== "publish")[0]
+  return label && images[label.node.name] || laptop
 }
