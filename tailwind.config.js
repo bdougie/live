2d7617f2370a524bde103ad4b8423e1b007e5636
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2020-10-25 16:00:08 -0700

    remove all purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: [
-    './components/**/*.{js,ts,jsx,tsx}',
-    './lib/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-  ],
   theme: {
     extend: {
       colors: {
