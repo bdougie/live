aab1ba16f0cbc9ff3b1a1a4bcc213a46a0f340be
Brian Douglas <bdougie@users.noreply.github.com>
2020-10-28 06:39:43 -0700

    Update features (#62)
    * removes the need for global react import
    
    * update the features
    
    * fix image url
    
    * fix image path

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,23 +1,28 @@
-const Feature = ({ title, description }) => {
+import imageUrl from '../imageUrl';
+
+const Feature = ({link, title, description}) => {
   return (
-    <div className="flex flex-col items-center text-center">
-      <svg
-        className="w-10 h-10"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+    <div>
+      <a href={link}>
+        <img
+          alt={title}
+          className="z-20 m-auto h-32 md:h-24"
+          src={`/${title.toLowerCase()}.png`}
         />
-      </svg>
-      <div className="mt-5">
-        <h5 className="text-lg font-bold leading-6">{title}</h5>
-        <p className="mt-2 text-base font-light leading-6">{description}</p>
+      </a>
+      <div
+        style={{minHeight: 215}}
+        className="-mt-14 bg-white rounded-lg p-6">
+        <div>
+          <a href={link}>
+            <h5 className="text-center text-purple-600 pt-8 text-lg font-bold leading-6">
+              {title}
+            </h5>
+          </a>
+          <p className="text-gray-600 p-4 mt-2 text-base font-light leading-6">
+            {description}
+          </p>
+        </div>
       </div>
     </div>
   );
